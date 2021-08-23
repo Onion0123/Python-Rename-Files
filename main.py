feef73da1e74f80a242c164bd0b23a9579671c02
jixing <75345063+j1xing@users.noreply.github.com>
2021-08-23 19:56:42 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# https://paste.ubuntu.com/p/PBDbmzw5FN/
+# 批量修改文件名
+import os
+
+path = 'D:/xxx\\xxx\\xxx'  # 文件地址,'\'这个符号不要忘了转义，要输入两个
+count = 0  # 可以自定义心得命名规则，可以用字符串
+for file_name in os.listdir(path):  # os.listdir(path) 返回一个文件名列表
+    if file_name[-3::] == 'jpg':  # 切片获取文件扩展名
+        count += 1  # 件名新文控制
+        new_name = str(count) + '.jpg'  # os.rename 的参数要写全文件路径
+        os.rename(path + '/' + file_name, path + '/' + new_name)
